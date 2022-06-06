f90777c18a6e6963bd61b152b33b93e8f66b8ced
kodaishiotsuki <0711dr5@gmail.com>
2022-06-06 15:28:11 +0900

    bootstrap導入

--- src/resources/views/event/index.blade.php
+++ src/resources/views/event/index.blade.php
@@ -1,11 +1,16 @@
-<h1>もくもく会一覧画面</h1>
-@foreach ($events as $event)
-    <p>{{ $event->event_id }}</p>
-    <p>{{ $event->category_id }}</p>
-    <p>{{ $event->title }}</p>
-    <p>{{ $event->date }}</p>
-    <p>{{ $event->start_time }}</p>
-    <p>{{ $event->end_time }}</p>
-    <p>{{ $event->content }}</p>
-    <p>{{ $event->entry_fee }}</p>
-@endforeach
+@extends('layouts.app')
+
+@section('content')
+    <h1>もくもく会一覧画面</h1>
+    @foreach ($events as $event)
+        <p>{{ $event->event_id }}</p>
+        <p>{{ $event->category_id }}</p>
+        <p>{{ $event->title }}</p>
+        <p>{{ $event->date }}</p>
+        <p>{{ $event->start_time }}</p>
+        <p>{{ $event->end_time }}</p>
+        <p>{{ $event->content }}</p>
+        <p>{{ $event->entry_fee }}</p>
+    @endforeach
+    <button class="btn btn-primary">検索</button>
+@endsection
