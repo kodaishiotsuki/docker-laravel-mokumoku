9da8184479937a80d92e855c26be4b508570b6f1
kodaishiotsuki <0711dr5@gmail.com>
2022-06-06 15:15:37 +0900

    categoriesテーブル MVC

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->category = new Category();
+    }
+
     /**
      * カテゴリー一覧画面
      */
-    public function index(){
-        return view('category.index');
+    public function index()
+    {
+        $categories = $this->category->allCategoriesData();
+        return view('category.index', compact('categories'));
     }
 }
--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $table = 'categories';
+
+    protected $primaryKey = 'category_id';
+
+    protected $fillable = [
+        'category_name',
+    ];
+
+    /**
+     * categoriesテーブルのレコードを全件取得
+     * 
+     * @param void
+     * @return Category categoriesテーブル
+     */
+
+    public function allCategoriesData()
+    {
+        return $this->get();
+    }
 }
